e1baf864ccc3ea621ce1401f4db77153ca9b086e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-03-02 19:12:25 +0100

    test4

--- app/page.js
+++ app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
             Freelance Designer and Developer
           </h1>
           {/* Buttons */}
-          <div className="flex gap-4 py-4">
+          <div className="flex gap-4 py-4 flex-wrap">
             <button className="px-6 py-4 md:px-8 md:py-4 rounded-3xl ring-1 ring-white bg-white text-black hover:bg-[#eee]">
               See Projects
             </button>
