311fe2e29c2a2ae077476463f5e52ccd62168efc
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-03-12 18:35:03 +0100

    New Changes

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,43 +1,58 @@
-"use client"
-import React, {useState} from "react";
-import {motion} from "framer-motion";
+"use client";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
-import gridPic from "../images/2.jpg"
-import secondGridPic from "../images/8.jpg"
-import thirdGridPic from "../images/10.png"
-import fourthGridPic from "../images/4.jpg"
-import fifthGridPic from "../images/3.png"
-import sixthGridPic from "../images/9.jpg"
+import gridPic from "../images/27.jpg";
+import secondGridPic from "../images/19.jpg";
+import thirdGridPic from "../images/12.jpg";
+import fourthGridPic from "../images/2.jpg";
+import fifthGridPic from "../images/4.jpg";
+import sixthGridPic from "../images/3.png";
 import ProjectItem from "../components/ProjectItem";
 
-// sm:px-8 md:px-12 lg:px-20 xl:px-32 max-w-[1920px] mx-auto lg:h-full 
-
-
+// sm:px-8 md:px-12 lg:px-20 xl:px-32 max-w-[1920px] mx-auto lg:h-full
 
 const page = () => {
-
-
   return (
     <div className="h-full px-4 py-4 max-w-[1920px] mx-auto lg:h-full sm:px-8">
-      
       <div className=" grid md:grid-cols-2 xl:grid-cols-3 h-full gap-4">
-        <ProjectItem title="Coffee Bar" backgroundImg={gridPic} projectUrl="/coffeebar" />
-        <ProjectItem title="Furniture Web App" backgroundImg={secondGridPic} projectUrl="/coffeebar" />
-        <ProjectItem title="Travel Website" backgroundImg={thirdGridPic} projectUrl="/coffeebar" />
-        <ProjectItem title="Food App" backgroundImg={fourthGridPic} projectUrl="/coffeebar" />
-        <ProjectItem title="Construction Website" backgroundImg={fifthGridPic} projectUrl="/coffeebar" />
-        <ProjectItem title="Sneaker Head" backgroundImg={sixthGridPic} projectUrl="/coffeebar" />
-
+        <ProjectItem
+          title="Furniture Web App"
+          backgroundImg={gridPic}
+          projectUrl="/furnico"
+        />
+        <ProjectItem
+          title="Sneakers Web App"
+          backgroundImg={secondGridPic}
+          projectUrl="/coffeebar"
+        />
+        <ProjectItem
+          title="Travel Website"
+          backgroundImg={thirdGridPic}
+          projectUrl="/coffeebar"
+        />
+        <ProjectItem
+          title="Coffee Bar Website"
+          backgroundImg={fourthGridPic}
+          projectUrl="/coffeebar"
+        />
+        <ProjectItem
+          title="Food Web App"
+          backgroundImg={fifthGridPic}
+          projectUrl="/foodwebapp"
+        />
+        <ProjectItem
+          title="Construction Website"
+          backgroundImg={sixthGridPic}
+          projectUrl="/cofreelance"
+        />
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default page
-
+export default page;
 
 // <div className="h-full grid grid-cols  md:grid-cols-2 xl:grid-cols-3 gap-4 px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32  max-w-[1920px] mx-auto">
 // <div className=" h-full w-full relative cursor-pointer hover:bg-black">
@@ -51,6 +66,5 @@ export default page
 // <Image priority src={fourthGridPic} alt="Grid Picture 4" width={300} className="object-cover h-full w-full" />
 // <Image priority src={fifthGridPic} alt="Grid Picture 5" width={300} className="object-cover h-full w-full" />
 // <Image priority src={sixthGridPic} alt="Grid Picture 6" width={300} className="object-cover h-full w-full" />
-
 
 // </div>
--- app/projects/second/page.jsx
+++ app/projects/second/page.jsx
@@ -1,16 +1,17 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import gridPic from "../../images/8.jpg";
+import gridPic from "../../images/15.jpg";
 import gridPic2 from "../../images/9.jpg";
-import gridPic6 from "../../images/6.jpg";
-import gridPic7 from "../../images/5.jpg";
-import gridPic8 from "../../images/6.jpg";
+import gridPic6 from "../../images/16.jpg";
+import gridPic7 from "../../images/17.jpg";
+import gridPic8 from "../../images/20.jpg";
 import gridPic9 from "../../images/7.jpg";
 import gridPic4 from "../../images/7.jpg";
 import FAQ from "../../../app/components/FAQ";
 import FAQ2 from "@/app/components/FAQ2";
 import Footer from "@/app/components/Footer";
+import Link from "next/link";
 
 const page = () => {
   return (
@@ -21,20 +22,21 @@ const page = () => {
         fill
         className="object-cover  bg-center"
       />
-      <div className="relative bg-black/40 max-w-[1920px] h-full flex flex-col justify-center items-center gap-6">
-        <p className="text-base leading-relaxed text-transform: uppercase">
-          Project Name
+      <div className="relative bg-black/80 max-w-[1920px] h-full flex flex-col justify-center items-center gap-4">
+        <p className="text-base leading-relaxed text-transform: uppercase text-center">
+          Project Info
         </p>
-        <h1 className="font-bold text-6xl items-center">
-          Furnico-Furniture <br /> Ecommerce Web App
+        <h1 className="font-bold text-6xl items-center text-center pb-4">
+          Sneaker Head <br />
+          Ecommerce Web App
         </h1>
         <button className="px-8 py-4 rounded-3xl ring-1 ring-white bg-white text-black hover:bg-[#eee]">
           Scroll Down
         </button>
       </div>
       {/* Project Info + Accordion */}
-      <div className="max-w-[1920px] mx-auto px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 gap-18 xl:py-24">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-16 lg:gap-24 xl:gap-48">
+      <div className="max-w-[1920px] h-screen mx-auto px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 gap-18  xl:py-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-16 lg:gap-24 py-8 sm:py-12 md:py-16 lg:py-24">
           <h2 className="text-5xl w-full font-bold">
             Project title that explains in short what was the project, type of
             project etc.
@@ -44,7 +46,7 @@ const page = () => {
           </div>
         </div>
         {/* Picture */}
-        <div className="max-w-[1920px] h-screen relative mx-auto px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 gap-18 xl:py-24 bg-[#ccc] gap-48">
+        <div className="max-w-[1920px] h-screen relative mx-auto gap-18 py-8 sm:py-12 md:py-16  lg:py-24">
           <Image
             priority={true}
             src={gridPic6}
@@ -53,60 +55,68 @@ const page = () => {
             alt="/"
           />
         </div>
-      </div>
-      {/* Picture with padding from side */}
-      <div className=" max-w-[1920px] h-screen mx-auto relative p-4 sm:p-8 md:py-16 gap-18 xl:py-24 gap-32">
-        <Image
-          priority={true}
-          src={gridPic2}
-          fill
-          className="object-cover max-w-full"
-          alt="/"
-        />
-      </div>
-      {/*Grid/Flex with 3 pictures */}
-      <div className="max-w-[1920px] h-screen mx-auto grid grid-cols md:grid-cols-2 gap-4 px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 xl:py-24">
-        <div className="relative col-span-2 ">
-          <Image
-            priority={true}
-            src={gridPic7}
-            fill
-            className="object-cover"
-            alt="/"
-          />
+        {/*Grid/Flex with 2 pictures */}
+        <div className="max-w-full h-screen relative  gap-24 sm:py-12 md:py-16 lg:py-24">
+          <div className="grid grid-cols md:grid-cols-2 h-full gap-4">
+            <div className="relative">
+              <Image
+                priority={true}
+                src={gridPic7}
+                fill
+                className="object-cover"
+                alt="/"
+              />
+            </div>
+
+            <div className="relative">
+              <Image
+                priority={true}
+                src={gridPic8}
+                fill
+                className="object-cover"
+                alt="/"
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="relative">
-          <Image
-            priority={true}
-            src={gridPic8}
-            fill
-            className="object-cover"
-            alt="/"
-          />
+        {/*Grid/Flex with 3 pictures */}
+        <div className="max-w-full h-screen mx-auto grid grid-cols md:grid-cols-2 gap-4 py-8 sm:py-12 md:py-16 lg:py-24">
+          <div className="relative sm:col-span-2 ">
+            <Image
+              priority={true}
+              src={gridPic7}
+              fill
+              className="object-cover max-w-full"
+              alt="/"
+            />
+          </div>
+
+          <div className="relative">
+            <Image
+              priority={true}
+              src={gridPic8}
+              fill
+              className="object-cover"
+              alt="/"
+            />
+          </div>
+          <div className="relative">
+            <Image
+              priority={true}
+              src={gridPic9}
+              fill
+              className="object-cover"
+              alt="/"
+            />
+          </div>
         </div>
-        <div className="relative">
-          <Image
-            priority={true}
-            src={gridPic9}
-            fill
-            className="object-cover"
-            alt="/"
-          />
+        <div className="max-w-full h-screen mx-auto relative gap-4">
+          <h2 className="text-5xl font-bold flex flex-col items-center justify-center h-[95%]">
+            <Link href="/projects/first">Next Case</Link>
+          </h2>
+          <Footer />
         </div>
-      </div>
-      {/*Huge picture with padding on the side within a container */}
-      <div className="max-w-[1920px] h-screen mx-auto relative gap-4 px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 xl:py-24">
-        <Image
-          priority={true}
-          src={gridPic7}
-          fill
-          className="object-cover max-w-full"
-          alt="/"
-        />
-      </div>
-      <div className="max-w-[1920px] h-screen mx-auto relative gap-4 px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 xl:py-24">
-        <Footer />
       </div>
     </div>
   );
