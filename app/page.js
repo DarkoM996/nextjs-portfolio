4dc12c9f6e6ca39d4c315e8e1127905bc8d8a858
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-03-14 12:53:03 +0100

    spacing homepage

--- app/page.js
+++ app/page.js
@@ -15,9 +15,9 @@ export default function Home() {
       transition={{ ease: "easeInOut", duration: 0.75 }}
       className="h-[calc(100vh-6rem)]"
     >
-      <div className="flex flex-col h-full lg:flex-row px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 lg:py-4 max-w-[1920px] mx-auto xl:space-x-20">
+      <div className="flex flex-col h-full lg:flex-row px-4 py-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 lg:py-4 max-w-[1920px] mx-auto xl:space-x-20 space-y-4">
         {/* Image Container */}
-        <div className="h-full lg:h-full lg:w-full relative">
+        <div className="h-full lg:h-full lg:w-full relative space-y-24">
           <Image
             priority
             src={heroPic}
