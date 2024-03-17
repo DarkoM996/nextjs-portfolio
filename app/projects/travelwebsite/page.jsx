ee53c6fc1427d20bd564345ea302532ca7f03dca
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-03-17 23:20:40 +0100

    latest

--- app/projects/travelwebsite/page.jsx
+++ app/projects/travelwebsite/page.jsx
@@ -4,7 +4,7 @@ import gridPic from "../../images/12.jpg";
 
 const page = () => {
   return (
-    <div className="max-w-[1920px] h-[103%] mx-auto relative">
+    <div className="max-w-[1920px] h-screen mx-auto relative">
       <Image
         priority={true}
         src={gridPic}
@@ -13,8 +13,8 @@ const page = () => {
         alt="/"
       />
       <div className="max-w-[1920px] h-[100%] mx-auto relative flex bg-black/50">
-        <h1 className="font-bold text-6xl w-full flex justify-center items-center">
-          Coming Soon
+        <h1 className="font-bold text-4xl md:text-6xl w-full flex flex-col justify-center items-center text-center">
+          Coming <br /> Soon
         </h1>
       </div>
     </div>
